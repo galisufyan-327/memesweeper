59827680d76a095a1e578badc458d64dd29ed6dd
Ali sufyan <alisufyan327@gmail.com>
2023-07-10 19:05:47 +0500

    add board model and controller

---
+++ db/migrate/20230710135327_create_boards.rb
@@ -0,0 +1,13 @@
+class CreateBoards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :boards do |t|
+      t.string :name
+      t.string :email
+      t.integer :mines
+      t.integer :height
+      t.inteegr :width
+
+      t.timestamps
+    end
+  end
+end
